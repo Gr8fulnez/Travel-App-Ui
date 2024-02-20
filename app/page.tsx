1755afd74ec266856d90bbb17d5a7aefcf142a78
Gr8fulnez <oluwolejohn72@gmail.com>
2024-02-20 23:10:40 +0100

    features,getapp, and footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import Camp from "@/components/Camp";
 import Features from "@/components/Features";
+import Footer from "@/components/Footer";
 import GetApp from "@/components/GetApp";
 import Guide from "@/components/Guide";
 import Hero from "@/components/Hero";
-import Image from "next/image";
 
 export default function Home() {
   return (
@@ -13,6 +13,7 @@ export default function Home() {
       <Guide />
       <Features/>
       <GetApp />
+      <Footer/>
     </>
   );
 }
--- components/Button.tsx
+++ components/Button.tsx
@@ -8,10 +8,10 @@ type ButtonProps = {
    variant: string;
   full?: boolean;
 };
-function Button({ title, icon, variant, type }: ButtonProps) {
+function Button({ title, icon, variant, type,full }: ButtonProps) {
   return (
     <button
-      className={`flexCenter gap-3 rounded-full border ${variant}`}
+          className={`flexCenter gap-3 rounded-full border ${variant} ${full && 'w-full'}`}
       type={type}
     >
       {icon && <Image src={icon} alt={title} width={24} height={24} />}
